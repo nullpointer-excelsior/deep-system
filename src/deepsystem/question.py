ca0d2506cdab820f11128df6c45165478daf83b3
Benjamin Team Rocket <benjamin@team.rocket.com>
2025-08-14 13:59:00 -0400

    refactor: update imports and enhance question handling in question.py

--- src/deepsystem/question.py
+++ src/deepsystem/question.py
@@ -1,8 +1,12 @@
-from system import SystemSummary, system_summary
+from system import system_summary
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.messages import HumanMessage
 from langchain.chat_models import init_chat_model
-
+from typing import Sequence
+from langchain_core.messages import HumanMessage, BaseMessage
+from langgraph.graph.message import add_messages
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import START, END, StateGraph
+from typing_extensions import Annotated, TypedDict
 
 system_prompt = """
 Eres un asistente especializado en sistemas operativos Linux, Bash scripting, programación en general, DevOps, y seguridad informática ofensiva y defensiva, así como en mejores prácticas en administración de sistemas y DevOps.
@@ -10,7 +14,7 @@ Eres un asistente especializado en sistemas operativos Linux, Bash scripting, pr
 # INSTRUCCIONES CLAVE:
 - Responderás todas las dudas que te presente en español, con respuesta directa.
 - Toda explicación debe ser breve y directa, con un enfoque técnico.
-- El código debe estar completamente en inglés, incluidos los nombres de variables, funciones y cualquier comentario necesario.
+- Si debes generar código este debe estar completamente en inglés, incluidos los nombres de variables, funciones, comandos y cualquier comentario necesario.
 - Incluye comentarios solo cuando sea estrictamente necesario para mejorar la claridad, siempre en inglés.
 
 # CONTEXTO
@@ -26,10 +30,61 @@ prompt_template = ChatPromptTemplate([
 
 
 llm = init_chat_model("gpt-4.1-nano-2025-04-14", model_provider="openai")
+
 question_model = prompt_template | llm
 
 messages = [
     HumanMessage(content="como instalo bat")
 ]
-for chunk in question_model.stream({ "messages": messages, "system_summary": system_summary.summary() }):
-    print(chunk.content, end="", flush=True)
+# for chunk in question_model.stream({ "messages": messages, "system_summary": system_summary.summary() }):
+#     print(chunk.content, end="", flush=True)
+
+
+class Options(TypedDict):
+    copy: bool
+    copy_code: bool
+
+
+class InputState(TypedDict):
+    question: str
+    options: Options
+
+
+class State(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], add_messages]
+    system_summary: str
+
+
+class OutputState(TypedDict):
+    answer: str
+
+
+def system_summary_node(state: InputState) -> State:
+    return {
+        "messages": [HumanMessage(content=state["question"])],
+        "system_summary": system_summary.summary()
+    }
+
+
+def model_call_node(state: State) -> OutputState:
+    answer = ''
+    for chunk in question_model.stream(state):
+        print(chunk.content, end="", flush=True)
+        answer += chunk.content
+    return {
+        "answer": answer
+    }
+
+def build_agent():
+    builder = StateGraph(state_schema=State, input_schema=InputState, output_schema=OutputState)
+    builder.add_node("system_summary", system_summary_node)
+    builder.add_node("model_call", model_call_node)
+    builder.add_edge(START, "system_summary")
+    builder.add_edge("system_summary", "model_call")
+    builder.add_edge("model_call", END)
+    return builder.compile()
+
+graph = build_agent()
+res = graph.invoke({ "question": "que sistema operativo es este?"})
+
+print(res)
