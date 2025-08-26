36c784039cee76cb7b6e03655fd44d251d53396e
Benjamin Team Rocket <benjamin@team.rocket.com>
2025-08-25 21:39:36 -0400

    refactor: remove unused clean_current_session function from sessions.py

--- src/deepsystem/sessions.py
+++ src/deepsystem/sessions.py
@@ -1,11 +1,5 @@
 from deepsystem.persistence import create_checkpointer
-from deepsystem.system import system_summary
 
-
-def clean_current_session():
-    thread_id = system_summary.cwd
-    checkpoint = create_checkpointer()
-    checkpoint.delete_thread(thread_id)
 
 def clean_session_by_thread_id(id):
     checkpoint = create_checkpointer()
